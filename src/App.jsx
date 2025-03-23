7f83dfaae5527d8323ed85dfd3391bcc173b1009
Denis Basenko <basenkodenis@gmail.com>
2025-03-23 17:37:57 +0200

    exercies part 0

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Exercise1_6__1_11 from "./part1/Unicafe1_6__1_11/Unicafe1_6__1_11"
 const App = () => {
     return (
       <div>
-      <Exercise1_1__1_5 /> {/* 1.1-1.5 */}
-      <Exercise1_6__1_11 /> {/* 1.6-1.11 */}
-      <Exercise1_12__1_14 /> {/* 1.12-1.14 */}
+      {/* <Exercise1_1__1_5 /> 1.1-1.5
+      <Exercise1_6__1_11 /> 1.6-1.11
+      <Exercise1_12__1_14 /> 1.12-1.14 */}
+
+      
     </div>
   )
 }
