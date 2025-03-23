647cec97d41d1d72bd5bd9ad3a1a7944a2a8d63c
Denis Basenko <basenkodenis@gmail.com>
2025-03-23 23:36:58 +0200

    exercies part 1

---
+++ src/part1/App.jsx
@@ -0,0 +1,19 @@
+import Exercise1_12__1_14 from "./Anecdotes1_12__1_14/Exercise1_12__1_14"
+import Exercise1_1__1_5 from "./CourseInfo1_1__1_5/Exercise1_1__1_5"
+import Exercise1_6__1_11 from "./Unicafe1_6__1_11/Unicafe1_6__1_11"
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Exercise1_1__1_5 /> 1.1-1.5
+      <Exercise1_6__1_11 /> 1.6-1.11
+      <Exercise1_12__1_14 /> 1.12-1.14
+
+
+    </div>
+  )
+}
+export default App
+
